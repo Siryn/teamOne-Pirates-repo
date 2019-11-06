1076094c82e2dd267efab1ca61f974dcf5be4f56
afletcher222 <55508413+afletcher222@users.noreply.github.com>
2019-11-06 15:50:51 -0500

    added offset to camera script

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -8,11 +8,12 @@ public class CameraController : MonoBehaviour
     public float yMax;
     public float xMin;
     public float yMin;
+    public float offset;
 
     public Transform target;
 
     void LateUpdate()
     {
-        transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin, xMax), Mathf.Clamp(target.position.y, yMin, yMax), transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp((target.position.x + offset), xMin, xMax), Mathf.Clamp(target.position.y, yMin, yMax), transform.position.z);
     }
 }
